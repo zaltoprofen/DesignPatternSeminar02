39fa350f23189dcb31d2f0b7ae504c8049d72867
Arudori5001 <kagaminaoto@kagaminaoto-no-iMac.local>
2016-10-18 20:36:26 +0900

    added Text class

---
+++ src/dpseminar02/ex/answer/Text.java
@@ -0,0 +1,27 @@
+package dpseminar02.ex.answer;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kagaminaoto on 2016/10/18.
+ */
+public class Text extends Tag {
+    private String content;
+
+    public Text(String content){
+        super(new ArrayList<Tag>());
+        this.content = content;
+    }
+
+    @Override
+    public void open() {}
+
+    @Override
+    public void close() {}
+
+
+    @Override
+    public void print(){
+        System.out.println(content);
+    }
+}
