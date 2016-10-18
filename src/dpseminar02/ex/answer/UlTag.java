96f25a1e70baf1c37ee712c938da66905f477839
Arudori5001 <kagaminaoto@kagaminaoto-no-iMac.local>
2016-10-18 20:37:21 +0900

    added UlTag class

---
+++ src/dpseminar02/ex/answer/UlTag.java
@@ -0,0 +1,23 @@
+package dpseminar02.ex.answer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kagaminaoto on 2016/10/18.
+ */
+public class UlTag extends Tag {
+    public UlTag(List<LiTag> children){
+        super(new ArrayList<Tag>(children));
+    }
+
+    @Override
+    public void open() {
+        System.out.println("<ul>");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("</ul>");
+    }
+}
