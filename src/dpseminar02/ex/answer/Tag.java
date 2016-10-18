bba7e3afbaece029ad679d58c4c58bb8930f2fee
Arudori5001 <kagaminaoto@kagaminaoto-no-iMac.local>
2016-10-18 20:36:08 +0900

    added Tag class

---
+++ src/dpseminar02/ex/answer/Tag.java
@@ -0,0 +1,25 @@
+package dpseminar02.ex.answer;
+
+import java.util.List;
+
+/**
+ * Created by kagaminaoto on 2016/10/18.
+ */
+public abstract class Tag {
+    private List<Tag> children;
+
+    public Tag(List<Tag> children) {
+        this.children = children;
+    }
+
+    public abstract void open();
+    public abstract void close();
+
+    public void print(){
+        open();
+        for(Tag child : children){
+            child.print();
+        }
+        close();
+    }
+}
